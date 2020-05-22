410c2860822cf0e4b4d9dbf385e493dcc6d7ae3e
goxr3plus <goxr3plus@gmail.com>
2020-05-22 15:17:53 +0300

    InputField and Add Button

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Button, TextInput, View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default App = () => {
+   return (
+      <View style={styles.screen}>
+         <View style={styles.inputContainer}>
+            <TextInput
+               placeholder="Course Goal"
+               style={{
+                  width: '80%',
+                  borderColor: 'black',
+                  borderWidth: 1,
+                  marginBottom: 5,
+                  padding: 10
+               }}
+            ></TextInput>
+            <Button title="ADD"></Button>
+         </View>
+         <View></View>
+      </View>
+   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+   screen: { paddingTop: 80 },
+   inputContainer: {
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+   }
+})
